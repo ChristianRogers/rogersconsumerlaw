83e62333109229446f80f0787b42ab7bfb843783
christianrogers <mariomanrogers@gmail.com>
2026-01-22 18:23:24 -0700

    Main page and some of the about page

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+//date scripts
+
 (function () {
   var yearElement = document.getElementById("current-year");
   if (yearElement) {
@@ -5,9 +7,29 @@
   }
 })();
 
+(function () {
+  var yearsElement = document.querySelectorAll(".years-in-operation");
+  if (yearsElement) {
+    var currentYear = new Date();
+    var MonthsSince =
+      (currentYear.getFullYear() - 1994) * 12 + (currentYear.getMonth() - 8);
+    yearsElement.forEach(function (element) {
+      element.textContent = Math.floor(MonthsSince / 12);
+    });
+  }
+})();
+
+// Navigation menu toggle
+
 navbutton = document.querySelector(".menu-toggle");
 navmenu = document.querySelector("nav");
 
 navbutton.addEventListener("click", function () {
   navmenu.classList.toggle("hidden");
+  // unhide aria
+  if (navmenu.classList.contains("hidden")) {
+    navbutton.setAttribute("aria-expanded", "false");
+  } else {
+    navbutton.setAttribute("aria-expanded", "true");
+  }
 });
